fa456f341aed17615212e19a9a2ab2fc34479412
michaelshorter <mail@mrshorter.co.uk>
2023-07-27 14:24:49 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-arecord --list-devices & sudo python3 main.py & cd AzureSpeechCC && /home/wordcloud/.dotnet/dotnet run & sleep 10s && sudo python3 write_to_oled.py
+arecord --list-devices & cd AzureSpeechCC && /home/wordcloud/.dotnet/dotnet run & sleep 10s && sudo python3 write_to_oled.py
