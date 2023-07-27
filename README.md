0190da015b9d5aa02c340380bc25060bf2fa8ab7
WordCloud <mail@mrshorter.co.uk>
2023-07-27 09:13:45 +0100

    clean up ignored files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Uberblick
-The new ChatGPT enabled version of the WordCloud
--- run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-arecord --list-devices & sudo python3 main.py & cd AzureSpeechCC && /home/wordcloud/.dotnet/dotnet run & sleep 10s && sudo python3 write_to_oled.py
