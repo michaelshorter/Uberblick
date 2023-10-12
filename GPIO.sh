c9fb3ac10bb640a6a4f9f49c2a49907f9570c5b9
michaelshorter <mail@mrshorter.co.uk>
2023-10-12 09:20:27 +0100

    Update GPIO.sh

--- GPIO.sh
+++ GPIO.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo python3 WiFiStatusLED.py & sudo python3 reset_button.py & sudo python3 ImageSelector.py & sudo python3 main.py 
+sudo python3 WiFiStatusLED.py & sudo python3 reset_button.py & sudo python3 ImageSelectorColour.py & sudo python3 main.py 
 
